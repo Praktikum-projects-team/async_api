8ba75cda5d01afb008ad42af3cc1e5d7d57d907c
Liana <liana.nigmatullina@tetrika.school>
2023-05-09 22:02:42 +0300

    fixture event_loop

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,10 +1,18 @@
 import aiohttp
 import pytest
+import asyncio
 
 from elasticsearch import AsyncElasticsearch
 
 from tests.functional.settings import test_settings
 from tests.functional.utils.helpers import get_es_bulk_query, create_index_if_not_exists, ApiResponse
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope='session')
@@ -14,26 +22,33 @@ async def es_client():
     await client.close()
 
 
-@pytest.fixture
-def es_write_data(es_client):
-    async def inner(index: str, data: list[dict]):
-        str_query = get_es_bulk_query(index, data)
+@pytest.fixture(scope='session')
+async def aiohttp_session():
+    async with aiohttp.ClientSession() as session:
+        yield session
 
+
+@pytest.fixture
+async def es_write_data(
+        es_client
+):
+    async def inner(index: str, data: list[dict]):
         await create_index_if_not_exists(es_client, index_name=index)
+
+        str_query = get_es_bulk_query(index, data)
         response = await es_client.bulk(str_query, refresh=True)
-        await es_client.close()
         if response['errors']:
             raise Exception('Ошибка записи данных в Elasticsearch')
+
     return inner
 
 
 @pytest.fixture
-def make_get_request():
+async def make_get_request(
+        aiohttp_session
+):
     async def inner(url: str, query_data: list[dict]):
-        session = aiohttp.ClientSession()
-        async with session.get(url, params=query_data) as response:
+        async with aiohttp_session.get(url, params=query_data) as response:
             resp = ApiResponse(status=response.status, body=await response.json())
-        await session.close()
         return resp
     return inner
-
--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,63 +1,31 @@
-# import pytest
-#
-#
-# @pytest.mark.asyncio
-# async def test_debug():
-#     pass
-#
-#
-# @pytest.mark.parametrize(
-#     'query_data, expected_answer',
-#     [
-#         (
-#                 {'search': 'The Star'},
-#                 {'status': 200, 'length': 50}
-#         ),
-#         (
-#                 {'search': 'Mashed potato'},
-#                 {'status': 200, 'length': 0}
-#         )
-#     ]
-# )
-# @pytest.mark.asyncio
-# async def test_search(es_write_data, query_data, expected_answer):
-#     es_data = [{
-#         # ....
-#     } for _ in range(60)]
-#
-#     await es_write_data(es_data)
-
-
-import datetime
-import json
-import uuid
-import orjson
-
-import aiohttp
 import pytest
-
-from elasticsearch import AsyncElasticsearch
-
-from tests.functional.conftest import es_write_data, make_get_request
+# from tests.functional.conftest import es_write_data, make_get_request
 from tests.functional.settings import test_settings
-from tests.functional.testdata.es_mapping import common_index_settings, index_mappings
 from tests.functional.testdata.search_film_collections import search_50_data
 
 
 @pytest.mark.parametrize(
-    'es_data',
-    [search_50_data]
+    'query_data, expected_answer',
+    [
+        (
+                {'query': 'The Star'},
+                {'status': 200, 'len': 50}
+        ),
+        (
+                {'query': 'Mashed potato'},
+                {'status': 200, 'len': 0}
+        )
+    ]
 )
 @pytest.mark.asyncio
-async def test_search(es_write_data, make_get_request, es_data):
-    await es_write_data(test_settings.es_index, es_data)
-
+async def test_search(es_write_data,
+                      make_get_request,
+                      query_data,
+                      expected_answer):
+    await es_write_data(test_settings.es_index, search_50_data)
     url = test_settings.service_url + '/api/v1/films/search'
-    query_data = {'query': 'The Star'}
 
     response = await make_get_request(url, query_data)
 
-    assert response.status == 200
-    assert len(response.body) == 50
-
-
+    assert response.status == expected_answer['status']
+    assert len(response.body) == expected_answer['len']
