1e7346753aa8b13efb11f095f1940995cc12d6d3
Liana <liana.nigmatullina@tetrika.school>
2023-05-09 15:43:42 +0300

    remove decorator nesting without parameters

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,9 @@
 import os
 from logging import config as logging_config
 from pydantic import BaseSettings, Field
-# from core.logger import LOGGING
+from core.logger import LOGGING
 
-# logging_config.dictConfig(LOGGING)
+logging_config.dictConfig(LOGGING)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- src/db/cache/cache_decorator.py
+++ src/db/cache/cache_decorator.py
@@ -6,15 +6,13 @@ from db.redis import redis
 redis_cache = RedisCache(redis)
 
 
-def with_cache():
-    def cache_decorator(method):
-        @functools.wraps(method)
-        async def wrapper(self, *args, **kwargs):
-            cache_key = self.cache.get_key(*args, **kwargs)
-            doc = await self.cache.get_cache(cache_key)
-            if not doc:
-                doc = await method(self, *args, **kwargs)
-                await self.cache.set_cache(key=cache_key, value=doc)
-            return doc
-        return wrapper
-    return cache_decorator
+def with_cache(method):
+    @functools.wraps(method)
+    async def wrapper(self, *args, **kwargs):
+        cache_key = self.cache.get_key(*args, **kwargs)
+        doc = await self.cache.get_cache(cache_key)
+        if not doc:
+            doc = await method(self, *args, **kwargs)
+            await self.cache.set_cache(key=cache_key, value=doc)
+        return doc
+    return wrapper
