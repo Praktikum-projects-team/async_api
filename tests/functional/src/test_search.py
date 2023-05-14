41446a12165b182e584e81256694cded2f87fc84
Liana <liana.nigmatullina@tetrika.school>
2023-05-15 00:20:54 +0300

    pytestmark pytest.mark.asyncio

--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -11,13 +11,13 @@ from tests.functional.testdata.genres_query import genres_search_query
 from tests.functional.testdata.films_query import films_search_query
 from tests.functional.testdata.persons_query import persons_search_query
 
+pytestmark = pytest.mark.asyncio
 
 @pytest.mark.parametrize(
     'query_data, diff_time, expected_answer', [
         *films_search_query
     ]
 )
-@pytest.mark.asyncio
 async def test_films_search(es_write_data, make_get_request, es_delete_data, query_data, diff_time, expected_answer):
     films_data = await get_films_data(DEFAULT_PAGE_SIZE)
     await es_write_data(EsIndex.MOVIES, films_data)
@@ -40,7 +40,6 @@ async def test_films_search(es_write_data, make_get_request, es_delete_data, que
         *genres_search_query
     ]
 )
-@pytest.mark.asyncio
 async def test_genres_search(es_write_data, make_get_request, es_delete_data, query_data, diff_time, expected_answer):
     genres = await get_genres_data(DEFAULT_PAGE_SIZE)
     await es_write_data(EsIndex.GENRE, genres)
@@ -62,7 +61,6 @@ async def test_genres_search(es_write_data, make_get_request, es_delete_data, qu
         *persons_search_query
     ]
 )
-@pytest.mark.asyncio
 async def test_persons_search(es_write_data, make_get_request, es_delete_data, query_data, diff_time, expected_answer):
     persons = await get_persons_data(DEFAULT_PAGE_SIZE)
     await es_write_data(EsIndex.PERSON, persons)
