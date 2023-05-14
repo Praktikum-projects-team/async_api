dd3f516f8415544e0c10ef65383400b51a39fdf9
Liana <liana.nigmatullina@tetrika.school>
2023-05-15 00:53:13 +0300

    BACKOFF IN WAITERS

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,18 +1,18 @@
-import logging
-import time
-
+import backoff
 import redis
 
 from tests.functional.settings import test_settings
 
+
+@backoff.on_exception(
+    backoff.expo,
+    redis.ConnectionError,
+    max_tries=10,
+)
+def wait_redis(redis_client):
+    redis_client.ping()
+
+
 if __name__ == '__main__':
     redis_client = redis.Redis(host=test_settings.redis_host, port=test_settings.redis_port, db=0)
-    while True:
-        try:
-            if not redis_client.ping():
-                continue
-            logging.info(f'Redis started {test_settings.redis_host}:{test_settings.redis_port}')
-            break
-        except redis.ConnectionError:
-            time.sleep(1)
-            logging.info('Waiting for Redis to start...')
+    wait_redis(redis_client)
